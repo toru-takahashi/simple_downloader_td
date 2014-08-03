0341058c3a5ef3646b333f23b2015e1516a2060a
toru-takahashi <torutakahashi.ayashi@gmail.com>
2014-08-03 09:55:47 +0900

    Release v0.0.1

--- lib/simple_downloader_td.rb
+++ lib/simple_downloader_td.rb
@@ -1,5 +1,72 @@
+# coding: utf-8
+require 'rubygems'
 require "simple_downloader_td/version"
+require "simple_downloader_td/command"
+require 'msgpack'
+require 'csv'
+require 'json'
+require 'td'
+require 'td-client'
+require 'zlib'
+require 'fileutils'
 
 module SimpleDownloaderTd
-  # Your code goes here...
+  def self.download(job_id)
+    # Get TD APIKEY in ~/.td/td.conf
+    data = File.read(File.join(ENV['HOME'], '.td', 'td-sios.conf'))
+    data.each_line do |line|
+      line.strip!
+      case line
+      when /^#/
+        next
+      when /\[(.+)\]/
+        section = $~[1]
+      when /^(\w+)\s*=\s*(.+?)\s*$/
+        key = $~[1]
+        val = $~[2]
+        @APIKEY = val
+      else
+        STDOUT.puts "Can't read apikey."
+      end
+    end
+    cln = TreasureData::Client.new(@APIKEY, {ssl: true})
+
+    # get job result
+    job = cln.job(job_id)
+
+    # check job status
+    unless job.finished? then
+      STDOUT.puts "The job didn't finish yet."
+      exit
+    end
+
+    # download job result as msgpack.gz
+    begin
+      File.open("#{job_id}_tmp.msgpack.gz", "wb") do |f|
+        job.result_format('msgpack.gz', f) do |compr_size|
+          STDOUT.puts "Downloaded: #{(compr_size / job.result_size) * 100}%"
+        end
+      end
+    rescue Exception => e
+      STDERR.puts e
+    end
+    STDOUT.puts "Start converting..."
+    # convert tsv file
+    begin
+      File.open("#{job_id}.tsv", "w+") do |writer|
+        src = MessagePack::Unpacker.new(Zlib::GzipReader.open("#{job_id}_tmp.msgpack.gz"))
+        job.hive_result_schema.each do |schema|
+          writer << schema[0] + "\t"
+        end
+        writer << "\n"
+        src.each do |obj|
+          writer << obj.join("\t") + "\n"
+        end
+      end
+      File.delete("#{job_id}_tmp.msgpack.gz")
+      STDOUT.puts "Finish."
+    rescue Exception => e
+      STDERR.puts e
+    end
+  end
 end
--- simple_downloader_td.gemspec
+++ simple_downloader_td.gemspec
@@ -8,9 +8,9 @@ Gem::Specification.new do |spec|
   spec.version       = SimpleDownloaderTd::VERSION
   spec.authors       = ["toru-takahashi"]
   spec.email         = ["torutakahashi.ayashi@gmail.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
-  spec.homepage      = ""
+  spec.summary       = %q{Download job result from TreasureData}
+  spec.description   = %q{Download job result from TreasureData}
+  spec.homepage      = "https://github.com/toru-takahashi/simple_downloader_td"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files -z`.split("\x0")
@@ -20,4 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
+  spec.add_dependency "td"
+  spec.add_dependency "td-client"
+  spec.add_dependency "msgpack"
 end
